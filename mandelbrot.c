f0f49f19fdd91306f3e1aca314f5ea8d533ae990
federicobozzini <federico.bozzini@gmail.com>
2018-02-03 11:23:41 +0100

    Added multiple trials for better time accuracy.

--- mandelbrot.c
+++ mandelbrot.c
@@ -7,24 +7,26 @@
 // USAGE: mandelbrot <m> <n>
 // OUTPUT: PERFORMANCE IN TIME SPENT
 
-#define TRIALS 10
+#define SEQ_TRIALS 3
 #define MAXTIME 5000000000 /* 5 seconds */
 
-double get_time() {
-	struct timespec tt;
+double get_time()
+{
+    struct timespec tt;
     clock_gettime(CLOCK_REALTIME, &tt);
-	double t = (double)tt.tv_sec*1.0e9 + (double)tt.tv_nsec;
+    double t = (double)tt.tv_sec * 1.0e9 + (double)tt.tv_nsec;
     return t;
 }
 
 int main(int argc, char **argv)
 {
     FILE *fp;
-    int m, n, size, i, j, px, py, iteration, max_iteration, *grid;
-    double x0, y0, x, y, xmin, xmax, ymin, ymax, xtemp, ttot, tstart, tend;
+    int m, n, size, i, j, k, px, py, iteration, max_iteration, *grid;
+    double x0, y0, x, y, xmin, xmax, ymin, ymax, xtemp, ttot, tstart, tend, tmin;
     char filename[] = "mandelbrot.dat";
 
-    if (argc < 3) {
+    if (argc < 3)
+    {
         printf("Usage: mandelbrot n m\n");
         return 1;
     }
@@ -32,7 +34,8 @@ int main(int argc, char **argv)
     n = atoi(argv[2]);
     size = m * n;
 
-    if (m < 2 || n < 2) {
+    if (m < 2 || n < 2)
+    {
         printf("Error: n and m must be > 2\n");
         return 1;
     }
@@ -45,32 +48,41 @@ int main(int argc, char **argv)
 
     grid = (int *)malloc(size * sizeof(int));
 
-    tstart = get_time();
-
-    for (i = 0; i < size; i++)
+    for (k = 0; k < SEQ_TRIALS; k++)
     {
-        px = i % m;
-        py = i / m;
-        x0 = (double)px / (m - 1) * (xmax - xmin) + xmin;
-        y0 = (double)py / (n - 1) * (ymax - ymin) + ymin;
-        x = 0;
-        y = 0;
-        iteration = 0;
-        while (x * x + y * y < 2 * 2 && iteration < max_iteration)
+        tmin = 10e10;
+
+        tstart = get_time();
+
+        for (i = 0; i < size; i++)
         {
-            xtemp = x * x - y * y + x0;
-            y = 2 * x * y + y0;
-            x = xtemp;
-            iteration++;
+            px = i % m;
+            py = i / m;
+            x0 = (double)px / (m - 1) * (xmax - xmin) + xmin;
+            y0 = (double)py / (n - 1) * (ymax - ymin) + ymin;
+            x = 0;
+            y = 0;
+            iteration = 0;
+            while (x * x + y * y < 2 * 2 && iteration < max_iteration)
+            {
+                xtemp = x * x - y * y + x0;
+                y = 2 * x * y + y0;
+                x = xtemp;
+                iteration++;
+            }
+            grid[i] = iteration;
         }
-        grid[i] = iteration;
+
+        tend = get_time();
+
+        ttot = tend - tstart;
+        printf("%i, time=%.2lf ms.\n", k, ttot / 10e6);
+
+        if (ttot < tmin)
+            tmin = ttot;
     }
 
-    tend = get_time();
-
-    ttot = tend - tstart;
-
-    printf("time=%.2lf ms.\n", ttot/10e6);
+    printf("time=%.2lf ms.\n", tmin / 10e6);
 
     fp = fopen(filename, "w");
 
