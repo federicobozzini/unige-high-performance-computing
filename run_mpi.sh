4afee9fa13ae18274dd22275c47cfc00d98fc3b5
federicobozzini <federico.bozzini@gmail.com>
2018-02-14 16:51:56 +0100

    Modified the script running th mpi program to automatically identify how many nodes are available in the cluster.

--- run_mpi.sh
+++ run_mpi.sh
@@ -3,10 +3,11 @@
 ROWS=${1:-1111}
 COLS=${2:-2222}
 TASK_SIZE=${3:-333}
-NODES=${4:-17}
+NODES=$(wc -l < macchine)
+NODES_USED=${4:-$NODES}
 X0=${5:--2.5}
 Y0=${6:--1}
 DX=${7:-3.5}
 DY=${8:-2}
 
-mpicc mandelbrot_mpi.c -fopenmp -lm -O3 -o mandelbrot_mpi && mpiexec -machinefile macchine -n $NODES ./mandelbrot_mpi $ROWS $COLS $TASK_SIZE $X0 $Y0 $DX $DY
+mpicc mandelbrot_mpi.c -fopenmp -lm -O3 -o mandelbrot_mpi && mpiexec -machinefile macchine -n $NODES_USED ./mandelbrot_mpi $ROWS $COLS $TASK_SIZE $X0 $Y0 $DX $DY
